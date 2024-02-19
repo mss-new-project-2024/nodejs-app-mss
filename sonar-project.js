71a9bd50d4259e938f7d019580dd13a327dde2ab
Md. Mehedi Hasan <155286161+mehedi8248@users.noreply.github.com>
2024-02-20 02:11:49 +0600

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://47.128.153.50:9000',
+  serverUrl: 'http://47.128.153.50:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMithunTechnologies',
-	    'sonar.login': 'squ_6ad58474c7a0a7a5c0651e10f159fc80d0208ccb',
+	    'sonar.login': 'squ_bf59fa6efa8893176b2a7835cd2cd3b5e92d8f8f',
 	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
